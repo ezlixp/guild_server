004b1614b3d896bb73b8959fa1349b33928593ac
pixlze <henrygoals2007@gmail.com>
2024-12-28 22:37:59 -0500

    init

---
+++ models/guildModel.ts
@@ -0,0 +1,17 @@
+import mongoose, { Model, Schema } from "mongoose";
+
+interface IGuild extends Document {
+    tag: String;
+    url: String;
+    validationKey: String;
+}
+
+const guildSchema: Schema<IGuild> = new Schema({
+    tag: { type: String, required: true },
+    url: { type: String, required: true },
+    validationKey: { type: String, required: true },
+});
+
+const GuildModel: Model<IGuild> = mongoose.model("Guild", guildSchema);
+
+export default GuildModel;
---
+++ security/passwordValidator.ts
@@ -0,0 +1,29 @@
+﻿import { Request, Response, NextFunction } from "express";
+
+/**
+ * Checks if the password provided in the request's headers is valid.
+ * If password is invalid, return status code 401 with an error message,
+ * else, return void.
+ */
+// Checks if the token provided in the request's headers is valid.
+// If token is invalid, return status code 401 with an error message.
+function validatePassword(request: Request, response: Response, next: NextFunction) {
+    const authorizationHeader = request.headers["authorization"] as string | undefined;
+
+    if (!authorizationHeader) {
+        response.status(401).json({ error: "No password provided" });
+        return;
+    }
+
+    // Get authorization headers and extract token from "Bearer <token>"
+    const password = authorizationHeader.split(" ")[1];
+
+    if (password !== process.env.PASSWORD) {
+        response.status(401).json({ error: "Invalid password provided" });
+        return;
+    }
+
+    next(); // Goes to next step (function execution)
+}
+
+export default validatePassword;
